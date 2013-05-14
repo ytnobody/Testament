d6bd1b3972b5dad381039d93a7ad28d3981135fc
ytnobody <ytnobody@gmail.com>
2013-05-14 12:04:05 +0900

    複数のIPチェッカーに対応させる

--- lib/Testament/FastestMirror.pm
+++ lib/Testament/FastestMirror.pm
@@ -6,11 +6,18 @@ use JSON;
 use URI;
 use Testament::URLFetcher;
 
-our $IPCHECKER_URL = 'http://ipsefact.ytnobody.net/api/json';
+our @IPCHECKER_URL_LIST = (
+    'http://ipsefact.ytnobody.net/api/json',
+);
+
+sub ip_checker_url {
+    my $class = shift;
+    return $IPCHECKER_URL_LIST[int(rand($#IPCHECKER_URL_LIST + 1)];
+}
 
 sub pickup {
     my ($class, @urllist) = @_;
-    my $ipinfo = JSON->new->utf8->decode(Testament::URLFetcher->get($IPCHECKER_URL));
+    my $ipinfo = JSON->new->utf8->decode(Testament::URLFetcher->get($class->ip_checker_url));
     my $my_country = uc($ipinfo->{country});
     my @near_url = grep {uc($_->host) =~ /\.$my_country$/} map {URI->new($_)} @urllist;
     return $near_url[int(rand($#near_url + 1))];
