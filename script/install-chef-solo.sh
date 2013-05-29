138e1aa9b2bed0e2cd123a72bfbabf0e20f0af04
ytnobody <ytnobody@gmail.com>
2013-05-29 11:40:31 +0900

    chefインストールスクリプト

--- script/install-chef-solo.sh
+++ script/install-chef-solo.sh
@@ -2,6 +2,9 @@
 
 PKG_ADD=$(which pkg_add)
 APT_GET=$(which apt-get)
+GEM=$(which gem)
+CHEF=$(which chef)
+KNIFE=$(which knife)
 
 die () {
     echo $1 >2&
@@ -16,9 +19,23 @@ if [ -z "$PKG_ADD" && -z "$APT_GET" ]; then
     die "apt-get and pkg_add not found"
 fi
 
-if [ ! -z "$PKG_ADD" ]; then
-    $PKG_ADD ruby
-    $PKG_ADD chef
-elif [ ! -z "$APT_GET" ]; then
-    $APT_GET install chef
+if [ -z "$GEM" ]; then 
+    if [ ! -z "$PKG_ADD" ]; then
+        $PKG_ADD ruby
+    elif [ ! -z "$APT_GET" ]; then
+        $APT_GET install ruby 
+    fi
+    GEM=$(which gem)
 fi
+
+if [ -z "$CHEF" ] ; then
+    $GEM i chef --no-ri --no-rdoc
+    CHEF=$(which chef)
+    KNIFE=$(which knife)
+    $KNIFE configure
+    $GEM i knife-solo --no-ri --no-rdoc
+    echo "done."
+else
+    echo "already installed."
+fi
+
