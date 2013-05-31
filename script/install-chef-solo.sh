7aef673169cd85eded0d5d70e23a8bc6e444ef5b
ytnobody <ytnobody@gmail.com>
2013-05-31 18:55:04 +0900

    git依存をなくした

--- script/install-chef-solo.sh
+++ script/install-chef-solo.sh
@@ -4,11 +4,8 @@ GEM=$(which gem)
 CHEF=$(which chef)
 KNIFE=$(which knife)
 
-RBENV_REPO=git://github.com/sstephenson/rbenv.git
 RBENV_ROOT=$HOME/.rbenv
 PROF_FILE=$HOME/.profile
-
-RUBYBUILDER_REPO=git://github.com/sstephenson/ruby-build.git
 RBENV_PLUGIN_DIR=$RBENV_ROOT/plugins
 
 die () {
@@ -22,18 +19,11 @@ write_profile () {
     fi
 }
 
-git --version || die "git is not installed."
-
 if [ -z "$GEM" ]; then 
     ### setup rbenv
-    git clone $RBENV_REPO $RBENV_ROOT || die "git clone failure : $RBENV_REPO"
     write_profile 'export RBENV_ROOT=$HOME/.rbenv'
     write_profile 'export PATH=$PATH:$RBENV_ROOT/bin'
     write_profile 'eval "$(rbenv init -)"'
-
-    ### setup ruby-builder
-    mkdir $RBENV_PLUGIN_DIR
-    git clone $RUBYBUILDER_REPO $RBENV_PLUGIN_DIR/ruby-build || die "git clone failure : $RUBYBUILDER_REPO"
 
     ### load profile
     . $PROF_FILE 
