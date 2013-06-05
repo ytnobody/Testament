67eb2fe73f0c6aa91aac7d834809038f79d73bcc
ytnobody <ytnobody@gmail.com>
2013-06-05 15:22:36 +0900

    fallbackではなく、httpsに寄せた

--- lib/Testament/Constants.pm
+++ lib/Testament/Constants.pm
@@ -11,8 +11,8 @@ our @EXPORT_OK = (qw(
 ));
 
 use constant CHEF_INSTALLER_URL => 'https://raw.github.com/ytnobody/Testament/master/script/install-chef-solo.sh';
-use constant RBENV_REPO         => 'git://github.com/sstephenson/rbenv.git';
-use constant RUBYBUILDER_REPO   => 'git://github.com/sstephenson/ruby-build.git';
+use constant RBENV_REPO         => 'https://github.com/sstephenson/rbenv.git';
+use constant RUBYBUILDER_REPO   => 'https://github.com/sstephenson/ruby-build.git';
 use constant SPAWN_TIMEOUT      => 15;
 
 1;
